6d89dd24d3986c9e6c972c84cb2f0f220fc8353b
tmuldoon21 <42975519+tmuldoon21@users.noreply.github.com>
2018-12-05 12:43:36 -0500

    Add files via upload

---
+++ src/formatingPractice.java
@@ -0,0 +1,17 @@
+
+public class formatingPractice 
+{
+
+	public static void main(String[] args) 
+	{
+		//double num = 17/6;
+		//System.out.printf("Please print %f. thank you.", num);
+		//double high = 53.8, low = 23.3;
+		//System.out.printf("The high of today us %f, and the low is %f.",high ,low);
+		//double num = 87416.12;
+		//System.out.printf("your pay is %,.2f.", num);
+		String name = "Tyler";
+		System.out.printf("Your name is %s.", name);
+	}
+
+}
